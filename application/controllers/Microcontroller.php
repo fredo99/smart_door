a637f3a9dba1fc8e77bd120137dc54cefa81eac8
fredo99 <59675091+fredo99@users.noreply.github.com>
2023-10-14 17:03:13 +0700

    Update Microcontroller.php

--- application/controllers/Microcontroller.php
+++ application/controllers/Microcontroller.php
@@ -5,7 +5,7 @@ class Microcontroller extends CI_Controller {
 
     public function __construct()
     {
-        $this->load->model('m_capacity');
+        $this->load->model('M_capacity');
     }
 
     public function cek_capacity()
