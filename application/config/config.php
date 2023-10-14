a832d306e6aae26406374543042d85974f566d74
fredo <fmaurtino@gmail.com>
2023-10-14 14:12:20 +0700

    base_url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/smart_door/';
-$config['base_url'] = 'http://smart-door.my.id/';
+$config['base_url'] = 'http://154.49.243.77/';
 
 /*
 |--------------------------------------------------------------------------
