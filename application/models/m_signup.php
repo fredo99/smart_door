c2a4a4612c16d731b639f47c8e3ed8d500dc03e8
fredo99 <59675091+fredo99@users.noreply.github.com>
2023-10-14 16:55:24 +0700

    Update m_signup.php

--- application/models/m_signup.php
+++ application/models/m_signup.php
@@ -1,13 +1,7 @@
 <?php
 class m_signup extends CI_Model
 {
-
-	public function __construct()
-    {
-        parent::__construct();
-    }
-
-	function check_id($id)
+   function check_id($id)
 	{
 		$query = $this->db->query("SELECT * FROM user WHERE id='$id' LIMIT 1");
 		return $query;
